a8c9f751f36a66213eb5426a62b87439abbb2742
doremi_xiangxuan <https://git.code.tencent.com/ceteng-duolaimi/ExamServer.git>
2020-04-20 14:28:09 +0800

    练习环境

---
+++ manual_practice/src/main/java/com/zxx/App.java
@@ -0,0 +1,13 @@
+package com.zxx;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
